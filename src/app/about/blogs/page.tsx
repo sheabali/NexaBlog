fd63b620278cba27f851879ca43f7e84e00f1fa1
sheabali <iamskpranto@gmail.com>
2025-02-22 14:36:36 +0600

    Show off all the fantastic blogs using SSR

--- src/app/about/blogs/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const BlogPage = () => {
-  return <div></div>;
-};
-
-export default BlogPage;
---
+++ src/app/blogs/loading.tsx
@@ -0,0 +1,13 @@
+'use client';
+import Spinner from '@/components/ui/Spinner';
+import React from 'react';
+
+const LoadingPage = () => {
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
+};
+
+export default LoadingPage;
