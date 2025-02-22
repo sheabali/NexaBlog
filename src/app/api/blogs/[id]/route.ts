99dd27c095aeba7f14aa72f926169307b4b3b125
sheabali <iamskpranto@gmail.com>
2025-02-23 00:34:26 +0600

    Exploring Route Handlers and Building RESTful APIs

---
+++ src/app/api/blogs/[id]/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+import { blogs } from '../route';
+
+export const GET = async (
+  request: Request,
+  params: { params: { id: string } }
+) => {
+  const { id } = params.params;
+
+  const blog = blogs.find((blog) => blog.id === id);
+
+  return NextResponse.json(blog);
+};
